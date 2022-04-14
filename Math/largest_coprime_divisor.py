438f15e5cde73a2503c984dd0ec44de90aedf6f7
Darshan Dodia <58351607+Darshan-D@users.noreply.github.com>
2022-04-14 21:47:49 +0530

    14th math

---
+++ Math/largest_coprime_divisor.py
@@ -0,0 +1,24 @@
+"""
+You are given two positive numbers A and B. You need to find the maximum valued integer X such that:
+
+X divides A i.e. A % X = 0
+X and B are co-prime i.e. gcd(X, B) = 1
+For example,
+
+A = 30
+B = 12
+We return
+X = 5
+"""
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def cpFact(self, A, B):
+        def gcd(A,B):
+            if B == 0:
+                return A
+            else:
+                return gcd(B, A%B)
+                
+        return A if gcd(A,B)==1 else self.cpFact(A//gcd(A,B),B)
